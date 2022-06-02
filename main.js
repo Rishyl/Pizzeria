3decb9b7f78496ce38e04f459ffbc69ad8208060
Rishyl <105464244+Rishyl@users.noreply.github.com>
2022-06-02 11:45:59 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+menu_list_array = [ "Cheesy Margherita Supreme Pizza",                  
+ "Meaty Madness Pizza",
+ "Vegan and Volatile Pizza",
+ "Crusty Crustacean Pizza",
+ "Ultimate Italian Pizza", 
+ "Foodies Flavor Custom Pizza"
+]; //Add more Pizza item names
+
+function getmenu()
+{
+    var htmldata="";
+    for(var i=0; i<menu_list_array.length; i++)
+    {
+        htmldata=htmldata+ menu_list_array[i] + '<br>'
+    }
+    //Give the appropriate id name as display_menu 
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_suggestion(){
+    //Give the appropriate id name as add_item
+	var item = document.getElementById("add_item").value;
+    //Use the push() function to push the item into menu_list_array
+    menu_list_array.     (item);
+}
